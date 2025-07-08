293741c65a77d9a720f4096ce5d409da0e823fa2
Guilherme Kodi Abe <guilherme.kodi.abe@gmail.com>
2025-07-08 01:05:10 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementation of https://www.tabnews.com.br/ in the most famous course in Brazil https://curso.dev/
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,3 @@
 export default function Home() {
-    return <h1>{`Você eh muito tipi <3`}</h1>
+  return <h1>{`Você eh muito tipi <3`}</h1>;
 }
